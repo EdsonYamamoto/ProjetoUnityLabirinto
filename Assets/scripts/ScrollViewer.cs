e6aa4c78dda62d28a47fe0ddeac632e8fd6a81c0
Edson Yamamoto <edsonkazyamamoto@gmail.com>
2019-08-27 19:05:07 -0300

    commit inicial

---
+++ Assets/scripts/ScrollViewer.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScrollViewer : MonoBehaviour
+{
+    public RectTransform prefab;
+    public ScrollRect scrollView;
+    public RectTransform content;
+
+    List<ExampleItemView> views = new List<ExampleItemView>();
+    // Start is called before the first frame update
+    void Start()
+    {
+
+
+        string json = "";
+
+        using (StreamReader r = new StreamReader("assets/armazenamentoDados/placar.json"))
+        {
+            json = r.ReadToEnd();
+        }
+
+        Scores result = JsonUtility.FromJson<Scores>(json);
+
+        OnReceivedNewModels(result.scores);
+
+    }
+
+
+    void OnReceivedNewModels(ScoreItem[] models)
+    {
+        int i = 0;
+
+        foreach (Transform child in content)
+        {
+            Destroy(child.gameObject);
+        }
+
+        views.Clear();
+        foreach (ScoreItem model in models)
+        {
+            GameObject instance = GameObject.Instantiate(prefab.gameObject) as GameObject;
+            instance.transform.SetParent(content, false);
+            ExampleItemView view = InitializeItemView(instance, model);
+
+            views.Add(view);
+            i++;
+        }
+    }
+
+    ExampleItemView InitializeItemView(GameObject viewGameObject, ScoreItem model)
+    {
+        ExampleItemView view = new ExampleItemView(viewGameObject.transform);
+
+        view.nome.text = model.Nome;
+        view.pontos.text = model.Pontos;
+
+        return view;
+    }
+
+    public class ExampleItemView
+    {
+
+        public Text nome;
+        public Text pontos;
+        public ExampleItemView(Transform rootView)
+        {
+            nome = rootView.Find("Nome").GetComponent<Text>();
+            pontos = rootView.Find("Pontos").GetComponent<Text>();
+        }
+    }
+
+    [Serializable]
+    public class ScoreItem
+    {
+        public string Nome;
+        public string Pontos;
+    }
+
+    [Serializable]
+    public class Scores
+    {
+        public ScoreItem[] scores;
+    }
+}
---
+++ Assets/scripts/timer.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class timer : MonoBehaviour
+{
+    public Text timerVisualizador;
+    public float tempo;
+    public bool jogoRodando;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            jogoRodando = !jogoRodando;
+        }
+
+        if (jogoRodando){
+
+            tempo += Time.deltaTime;
+            timerVisualizador.text = tempo.ToString("0.00") + "s";
+
+        }
+    }
+}
